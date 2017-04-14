59cf0fee4aa65a0c4cb800b2ad89c33418f84f52
liuhong <liuhong@idealsee.cn>
2017-04-14 13:43:55 +0800

    update

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../lib/index.js';
+
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+
+            </Layout>
+        );
+    }
+}
+
+export default App;
